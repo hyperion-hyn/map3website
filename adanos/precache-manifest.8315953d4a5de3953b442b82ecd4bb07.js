dbb463dc27d44077ab394e13b58100a7abdf791e
Moo <moyaying88@gmail.com>
2019-02-28 16:07:54 +0800

    adanos - benchmark

---
+++ adanos/precache-manifest.8315953d4a5de3953b442b82ecd4bb07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d954e6054770c1c9549",
+    "url": "/adanos/static/css/main.e09371c2.chunk.css"
+  },
+  {
+    "revision": "2d954e6054770c1c9549",
+    "url": "/adanos/static/js/main.2d954e60.chunk.js"
+  },
+  {
+    "revision": "42b40be9a19a3e14144b",
+    "url": "/adanos/static/js/runtime~main.42b40be9.js"
+  },
+  {
+    "revision": "46ecbff22f5c7dcdf556",
+    "url": "/adanos/static/js/2.46ecbff2.chunk.js"
+  },
+  {
+    "revision": "88c2397396757d3d12cfab2740f19c97",
+    "url": "/adanos/index.html"
+  }
+];
